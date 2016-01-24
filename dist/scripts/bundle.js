8fc28ce99236d8114fba0078895ecb26c9ccb01a
Maycon Beserra <mayconbeserra@hotmail.com>
2016-01-24 00:54:28 -0200

    Add a check when leaving the page

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -47028,14 +47028,24 @@ var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
         Router.Navigation
     ],
 
+    statics: {
+        willTransitionFrom: function(transition, component) {
+            if (component.state.dirty && !confirm('Leave without saving?')) {
+                transition.abort();
+            }
+        }
+    },
+
     getInitialState: function() {
         return {
             author: { id: '', firstName: '', lastName: '' },
-            errors: {}
+            errors: {},
+            dirty: false
         };
     },
 
     setAuthorState: function(event) {
+        this.setState({dirty: true});
         var field = event.target.name;
         var value = event.target.value;
         this.state.author[field] = value;
@@ -47068,6 +47078,7 @@ var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
         }
 
         AuthorApi.saveAuthor(this.state.author);
+        this.setState({dirty: false});
         toastr.success('Author saved.');
         this.transitionTo('authors');
     },
