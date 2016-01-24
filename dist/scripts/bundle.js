493538577b0f534c1dcff81a627fcf05913f45ed
Maycon Beserra <mayconbeserra@hotmail.com>
2016-01-24 00:44:15 -0200

    Add some propTypes validation

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -46907,6 +46907,13 @@ var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({displayName: "AuthorForm",
 
+    propTypes: {
+        author: React.PropTypes.object.isRequired,
+        onSave: React.PropTypes.func.isRequired,
+        onChange: React.PropTypes.func.isRequired,
+        errors: React.PropTypes.object
+    },
+
     render: function() {
         return (
             React.createElement("form", null, 
